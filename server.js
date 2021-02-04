a1e069731c2faff267abee4d8c4952e737996bfd
AntoineAmilien <47785228+AntoineAmilien@users.noreply.github.com>
2021-02-04 13:45:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mangoUrl = process.env.MANGODB_URL 
-
+console.log(mangoUrl);
 mongoose.connect(mangoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
