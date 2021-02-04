a4caa80ddf5be9e2333fa7d2eaa5f14a90e9ee61
AMILIEN Antoine <amilienantoine.dev@gmail.com>
2021-02-04 14:07:10 +0100

    marche 1

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 const app = express();
 
 //connexion a mangoDb
-//const db = require("./app/models");
+const db = require("./app/models");
+/*
+module.exports = {
+  url: "mongodb+srv://root:toor@cluster1.9jij8.mongodb.net/MyFit?retryWrites=true&w=majority"
+};*/
+
 
 var corsOptions = {
   origin: "*"
@@ -20,7 +25,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const mangoUrl = process.env.MANGODB_URL 
 
-mongoose.connect(mangoUrl, {
+db.mongoose
+.connect(db.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -31,7 +37,6 @@ mongoose.connect(mangoUrl, {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
-
 
 // Declaration du fichier avec les routes personalisées pour le controller tutoriel
 require("./app/routes/programme.routes")(app);
